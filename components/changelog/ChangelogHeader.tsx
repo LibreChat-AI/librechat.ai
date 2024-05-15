1e2c133537a94775a539427a77e5705db2af4cf9
fuegovic <fueg@live.ca>
2024-05-15 14:38:32 -0400

    update changelog

--- components/changelog/ChangelogHeader.tsx
+++ components/changelog/ChangelogHeader.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { Page } from 'nextra'
 import { getPagesUnderRoute } from 'nextra/context'
-import Link from 'next/link'
 import { Author } from '../Author/Authors'
 import { Video } from '../Video'
 
@@ -25,13 +24,6 @@ export const ChangelogHeader = () => {
 
   return (
     <div className="md:mt-10 flex flex-col gap-10">
-      <Link
-        href={`/changelog${page.route ? '#' + page.route.replace('/changelog/', '') : ''}`}
-        className="md:mb-10"
-      >
-        ← Back to changelog
-      </Link>
-
       <div>
         <div className="text-lg text-primary/60 mb-3">
           {new Date(date).toLocaleDateString('en-US', {
