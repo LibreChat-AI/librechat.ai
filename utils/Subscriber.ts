3a04fe9c6578ec16b5bdce5f6cc924f3b350a641
Danny Avila <danny@librechat.ai>
2025-02-20 14:21:18 -0500

    🔨 refactor: Migrate Subscriber model to TypeScript and update API handler for type safety

---
+++ utils/Subscriber.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Model, Document } from 'mongoose'
+
+interface ISubscriber extends Document {
+  email: string
+  status: string
+}
+
+// Define the schema
+const SubscriberSchema = new Schema({
+  email: {
+    type: String,
+    lowercase: true,
+    required: true,
+    unique: true,
+  },
+  status: {
+    type: String,
+    default: 'subscribed',
+  },
+})
+
+const Subscriber: Model<ISubscriber> =
+  mongoose.models.Subscriber || mongoose.model<ISubscriber>('Subscriber', SubscriberSchema)
+
+export default Subscriber
